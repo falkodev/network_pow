15fc7d73ce7acf98d5317787e569e96b02329b0f
Anthony Tarlao <anthony.tarlao@gmail.com>
2015-06-04 17:12:36 +0200

    Load forms service into a different file

--- src/AppBundle/DependencyInjection/AppExtension.php
+++ src/AppBundle/DependencyInjection/AppExtension.php
@@ -21,5 +21,6 @@ class AppExtension extends Extension
     {
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.yml');
+        $loader->load('forms.yml');
     }
 }
